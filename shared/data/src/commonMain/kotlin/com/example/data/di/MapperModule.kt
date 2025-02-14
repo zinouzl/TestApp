bb6e93530aa4652a4f64f27fc2a844ba0efdbf16
Zine Eddine <latiou_z@modulotech.fr>
2025-02-14 18:02:51 +0100

    fix: mapper bug

--- shared/data/src/commonMain/kotlin/com/example/data/di/MapperModule.kt
+++ shared/data/src/commonMain/kotlin/com/example/data/di/MapperModule.kt
@@ -7,9 +7,13 @@ import com.example.data.mapper.PostMapper
 import com.example.data.mapper.UserMapper
 import com.example.domain.model.Post
 import com.example.domain.model.User
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
+const val POST_MAPPER_QUALIFIER = "POST_MAPPER_QUALIFIER"
+const val USER_MAPPER_QUALIFIER = "USER_MAPPER_QUALIFIER"
+
 val mapperModule = module {
-    single<Mapper<PostEntity, Post>> { PostMapper() }
-    single<Mapper<UserEntity, User>> { UserMapper() }
+    single<Mapper<PostEntity, Post>>(named(POST_MAPPER_QUALIFIER)) { PostMapper() }
+    single<Mapper<UserEntity, User>>(named(USER_MAPPER_QUALIFIER)) { UserMapper() }
 }
--- shared/data/src/commonMain/kotlin/com/example/data/di/RepositoryModule.kt
+++ shared/data/src/commonMain/kotlin/com/example/data/di/RepositoryModule.kt
@@ -4,10 +4,11 @@ import com.example.data.repository.AuthRepositoryImpl
 import com.example.data.repository.PostRepositoryImpl
 import com.example.domain.repository.AuthRepository
 import com.example.domain.repository.PostRepository
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 
 val repositoryModule = module {
-    single<AuthRepository> { AuthRepositoryImpl(get(), get()) }
-    single<PostRepository> { PostRepositoryImpl(get(), get()) }
+    single<AuthRepository> { AuthRepositoryImpl(get(), get(named(USER_MAPPER_QUALIFIER))) }
+    single<PostRepository> { PostRepositoryImpl(get(), get(named(POST_MAPPER_QUALIFIER))) }
 }
